eccd1cf3783589ea06660693c6f8a51275454eaf
keshrisourav <91777401+keshrisourav@users.noreply.github.com>
2021-10-02 15:29:32 +0530

    assigment

---
+++ assigment - Copy/assignment.py
@@ -0,0 +1,47 @@
+#Question 1
+
+x = 2
+y = 5
+xy = 'Hey'
+print (x+y, xy)
+
+#Question 2
+print("""My name is Rajath Kumar M.P.
+
+I love Python.""")
+
+
+
+#Question 3
+
+string1 = 'World'
+print('Hello', string1)
+
+string2 = '!'
+print('Hello', string1, string2)
+
+
+
+#Question 4
+print('Hello' + string1 + string2)
+
+
+#Question 5
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
+
+#Question 6
+print("I want \\n to be printed.")
+
+#Question 7
+x = 10
+print(type(x))
+ 
+s = 'abc'
+print(type(s))
+ 
+value= True
+print(type(value))
+
+percent=90.00
+type(percent)
